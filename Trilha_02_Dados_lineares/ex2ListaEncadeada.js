b4266f2fa7b3b7fd9d3eff79fe5e61c7f6beee21
| Felipe Rafael Rothbarth | <108741421+rothibarth@users.noreply.github.com>
2025-03-06 20:19:09 -0300

    new

--- Trilha_02_Dados_lineares/ex2ListaEncadeada.js
+++
@@ -1,8 +0,0 @@
-// Lista de Exercícios de Fixação
-
-
-// 2. Lista Simplesmente Encadeada:
-
-// ○ Implemente uma lista simplesmente encadeada com as seguintes operações:
-// inserir no início, inserir no final e remover de uma posição específica.
-// ○ Modifique o código anterior para permitir a busca de um elemento por valor
