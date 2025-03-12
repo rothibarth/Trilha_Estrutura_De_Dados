1010eece7539c66e82514776f6e553b1230c432f
| Felipe Rafael Rothbarth | <108741421+rothibarth@users.noreply.github.com>
2025-03-11 22:04:39 -0300

    new

---
+++ Trilha_Estrutura_De_Dados/Trilha_03_Recursividade/Ex3_Travessia_em_Arvores/ex3_travessia_arvores.js
@@ -0,0 +1,5 @@
+// 3. Travessia em Árvores:
+// ○ Implemente um algoritmo recursivo para realizar o percurso in-order de uma
+// árvore binária.
+// ○ Altere o código para implementar os percursos pre-order e post-order.
+
