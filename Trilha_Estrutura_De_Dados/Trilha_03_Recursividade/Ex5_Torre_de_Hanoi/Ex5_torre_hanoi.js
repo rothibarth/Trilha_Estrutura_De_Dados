a8ef62b0baff4f6b0fb7fc1f2875e6c2c8810731
| Felipe Rafael Rothbarth | <108741421+rothibarth@users.noreply.github.com>
2025-03-13 19:30:27 -0300

    new

---
+++ Trilha_Estrutura_De_Dados/Trilha_03_Recursividade/Ex5_Torre_de_Hanoi/Ex5_torre_hanoi.js
@@ -0,0 +1,6 @@
+// 6. Torre de Hanói:
+// ○ Implemente o algoritmo recursivo para resolver o problema da Torre de
+// Hanói, movendo nnn discos de uma haste para outra.
+// ○ Determine o número de movimentos necessários para resolver o problema e
+// sua complexidade de tempo.
+
